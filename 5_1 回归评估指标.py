76a400c3a096cd156040322c8940ae64d4923e94
123 <2066868678@qq.com>
2026-06-03 10:35:45 +0800

    模型评估

---
+++ 5_1 回归评估指标.py
@@ -0,0 +1,6 @@
+﻿import numpy as np
+y_true = np.array([12.3, 15.1, 9.8, 20.5, 14.2])
+y_pred = np.array([11.8, 15.9, 10.2, 19.7, 14.6])
+mse = np.mean((y_true - y_pred) ** 2)
+rmse = np.sqrt(mse)
+print(f'RMSE= {rmse:.4f}')
---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Hello World 演示程序"""
+
+def main():
+    """主函数：输出 Hello World"""
+    print("Hello, World!")
+    print("你好，世界！")
+
+
+if __name__ == "__main__":
+    main()
